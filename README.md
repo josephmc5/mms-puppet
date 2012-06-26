06377ff81364f5b2a164207337982a42637f6b7e
Joe McWilliams <josephmc5@gmail.com>
2012-06-26 16:48:48 -0400

    initial commit

--- README.md
+++ README.md
@@ -2,3 +2,5 @@ mms-puppet
 ==========
 
 puppet module to setup and run the mms-agent for mongodb
+
+MMS requires an agent that is generated from the online interface. Login to mms.10gen.com and download the agent tar.gz. Drop your agent tar.gz into the files directory. 
---
+++ manifests/init.pp
@@ -0,0 +1,36 @@
+class mms {
+	file { "/root/10gen-mms-agent.tar.gz":
+        ensure => present,
+		source => "puppet:///modules/mms/10gen-mms-agent.tar.gz",
+	}
+    file { "/opt":
+        ensure => directory,
+    }
+    package { "pymongo":
+        ensure => present,
+        provider => pip,
+    }
+    exec { "tar zxf /root/10gen-mms-agent.tar.gz -C /opt":
+        creates => "/opt/mms-agent/agent.py",
+        require => [File["/root/10gen-mms-agent.tar.gz"], Package["pymongo"]],
+        path    => ['/bin/', '/usr/bin/'],
+    }
+    file { "/var/log/mms":
+        ensure => directory,
+    }
+    file { "/etc/init/mms-agent.conf":
+        ensure => present,
+        source => "puppet:///modules/mms/mms-agent.init",
+        mode    => "755",
+    }
+    file { "/etc/init.d/mms-agent":
+        ensure  => link,
+        target  =>  "/lib/init/upstart-job",
+        require => File["/etc/init/mms-agent.conf"],
+    }
+	service {"mms-agent":
+	    ensure => running,
+	    enable => true,
+        require => [File["/var/log/mms", "/etc/init/mms-agent.conf"], Exec["tar zxf /root/10gen-mms-agent.tar.gz -C /opt"]],
+	}
+}
